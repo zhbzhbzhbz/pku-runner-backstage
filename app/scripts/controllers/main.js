2afcbbd3014006611979d9fe55571da3f13c3144
cnmozzie <cnmozzie@gmail.com>
2017-03-20 16:19:59 +0800

    Using yo to create a angular framework

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pkuRunnerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pkuRunnerApp
+ */
+angular.module('pkuRunnerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
