15451414457aed5d2a7945be19222dc3da1a8224
cnmozzie <cnmozzie@gmail.com>
2017-03-22 15:01:10 +0800

    Add the welcom page

---
+++ app/scripts/controllers/admin.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pkuRunnerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pkuRunnerApp
+ */
+angular.module('pkuRunnerApp')
+    .controller('AdminCtrl', ['$scope', 'recordFactory', 'userFactory', function ($scope, recordFactory, userFactory) {
+        
+        $scope.showTable = false;
+        $scope.message="Loading ...";
+        
+        recordFactory.get().$promise.then(
+            function (response) {
+                $scope.records = response.data;
+                $scope.showTable = true;
+            },
+            function (response) {
+                $scope.message = "Error: " + response.status + " " + response.statusText;
+                console.log($scope.message);
+            });
+        
+        $scope.showTableB = false;
+        $scope.messageB="Loading ...";
+        
+        userFactory.get().$promise.then(
+            function (response) {
+                $scope.users = response.data;
+                $scope.showTableB = true;
+            },
+            function (response) {
+                $scope.messageB = "Error: " + response.status + " " + response.statusText;
+                console.log($scope.messageB);
+            });
+
+    }]);
