91bdb65e10adb888797f9704d43c1ad2d648a89b
CathyMouse96 <cathy_teng@hotmail.com>
2017-06-06 19:57:05 +0800

    add total distance ran for users

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -38,6 +38,10 @@ angular.module('pkuRunnerApp')
         $scope.allCourses = [];
         $scope.allTeachers = [];
         $scope.userRecords = [];
+        $scope.userDistance = {};
+        $scope.idToString = function(id) {
+            return String(id);
+        };
 
         const allStr = '------全部------';
         $scope.message='Loading ...';
@@ -65,6 +69,16 @@ angular.module('pkuRunnerApp')
                         recordsSortByUser[userIdString] = [];
                     }
                     recordsSortByUser[userIdString].push(records[i]);
+                    // Add to total distance of user
+                    // Only add if record is verified
+                    if (records[i].verified) {
+                        if (userDistance.hasOwnProperty(userIdString)) {
+                            userDistance[userIdString] += records[i].distance;
+                        }
+                        else {
+                            userDistance[userIdString] = records[i].distance;
+                        }
+                    }
                 }
                 
                 $scope.recordsSortByUser = recordsSortByUser;
