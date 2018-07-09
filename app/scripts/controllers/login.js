defefead324684a9b77940720320beeb31db6b56
mozzie <mozzie@localhost.localdomain>
2018-07-09 23:13:13 +0800

    fix the admin login on new sever

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,15 +11,15 @@ angular.module('pkuRunnerApp')
     .controller('LoginCtrl', ['$scope', '$location', '$localStorage', 'AdminAuthFactory', '$state', '$rootScope', 'ngDialog', function ($scope, $location, $localStorage, AdminAuthFactory, $state, $rootScope, ngDialog) {
 
         $scope.loginData = $localStorage.getObject('admininfo','{}');
-    
+
         $scope.doLogin = function() {
             if($scope.rememberMe)
                 $localStorage.storeObject('admininfo',$scope.loginData);
 
-            AdminAuthFactory.preLogin($scope.preLoginData);
+            AdminAuthFactory.login($scope.loginData);
             ngDialog.close();
-            
+
 
     };
-            
+
 }]);
--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -2,52 +2,52 @@
 
 angular.module('pkuRunnerApp')
 
-        .constant("baseURL", "http://pkuzone.jios.org:10201/")
+        .constant("baseURL", "https://pkunewyouth.pku.edu.cn/")
 
         .factory('statusFactory', ['$resource', '$http', 'baseURL', function($resource, $http, baseURL) {
-            
+
             // Set the token so you can login as Gao Kun
             //$http.defaults.headers.common['Authorization'] = '0bdc1af847e3357ec22bcaafbf508a32';
-            
+
             return $resource(baseURL + 'record/status/:userId', null, {
                 'update': {
                     method: 'PUT'
                 }
             });
-    
+
         }])
 
         .factory('recordFactory', ['$resource', '$http', 'baseURL', function($resource, $http, baseURL) {
-            
+
             // Set the token so you can login as Gao Kun
             //$http.defaults.headers.common['Authorization'] = '0bdc1af847e3357ec22bcaafbf508a32';
-            
+
             return $resource(baseURL + 'record/:userId/:recordId', null, {
                 'update': {
                     method: 'PUT'
                 }
             });
-    
+
         }])
 
         .factory('adminRecordFactory', ['$resource', '$http', 'baseURL', function($resource, $http, baseURL) {
-            
+
             return $resource(baseURL + 'admin/record', null, {
                 'update': {
                     method: 'PUT'
                 }
             });
-    
+
         }])
 
         .factory('adminUserFactory', ['$resource', '$http', 'baseURL', function($resource, $http, baseURL) {
-            
+
             return $resource(baseURL + 'admin/user', null, {
                 'update': {
                     method: 'PUT'
                 }
             });
-    
+
         }])
 
         .factory('$localStorage', ['$window', function ($window) {
@@ -71,7 +71,7 @@ angular.module('pkuRunnerApp')
         }])
 
         .factory('AuthFactory', ['$resource', '$http', '$localStorage', '$rootScope', '$window', 'baseURL', 'ngDialog', function($resource, $http, $localStorage, $rootScope, $window, baseURL, ngDialog){
-    
+
             var authFac = {};
             var TOKEN_KEY = 'userinfo';
             var isAuthenticated = false;
@@ -81,7 +81,7 @@ angular.module('pkuRunnerApp')
             var department = "";
             var isPESpecialty = false;
             var identityType = "";
-    
+
 
             function loadUserCredentials() {
                 console.log("loadUserCredentials");
@@ -90,24 +90,24 @@ angular.module('pkuRunnerApp')
                     useCredentials(credentials);
                 }
             }
- 
+
             function storeUserCredentials(credentials) {
                 console.log("storeUserCredentials");
                 $localStorage.storeObject(TOKEN_KEY, credentials);
                 useCredentials(credentials);
             }
- 
+
             function useCredentials(credentials) {
                 console.log("useCredentials");
                 console.log(credentials);
                 isAuthenticated = true;
                 id = credentials.id;
                 authToken = credentials.token;
- 
+
                 // Set the token as header for your requests!
                 $http.defaults.headers.common['Authorization'] = authToken;
             }
- 
+
             function destroyUserCredentials() {
                 authToken = undefined;
                 id = 0;
@@ -117,7 +117,7 @@ angular.module('pkuRunnerApp')
                 console.log("destroyUserCredentials");
                 console.log("isAuthenticated: " + isAuthenticated);
             }
-     
+
             authFac.login = function(loginData) {
                 console.log(loginData);
                 $resource(baseURL + "user")
@@ -131,7 +131,7 @@ angular.module('pkuRunnerApp')
                                     window.location.href="http://pkuzone.jios.org:10201/dist/#!/student";
                                 else if(response.data.identityType = "职工")
                                     window.location.href="http://pkuzone.jios.org:10201/dist/#!/faculty";
-                                else 
+                                else
                                     console.log("who you are?");
                             }
                             else {
@@ -140,51 +140,51 @@ angular.module('pkuRunnerApp')
 
                                 window.location.href="http://pkuzone.jios.org:10201/dist/";
 
-                                
+
                             }
-                            
+
                           },
                           function(response){
                             console.log(response);
                             isAuthenticated = false;
-            
+
                             var message = '<div class="ngdialog-message"><div><h3>Login Unsuccessful</h3></div>' +'<div><p>' +  response.data.err.message + '</p><p>' + response.data.err.name + '</p></div>' + '<div class="ngdialog-buttons"><button type="button" class="ngdialog-button ngdialog-button-primary" ng-click=confirm("OK")>OK</button></div>';
-            
+
                             ngDialog.openConfirm({ template: message, plain: 'true'});
                           }
-        
+
                          );
 
             };
-    
+
             authFac.logout = function() {
                 //$resource(baseURL + "users/logout").get(function(response){
                 //});
                 destroyUserCredentials();
             };
-    
-    
+
+
             authFac.isAuthenticated = function() {
                 return isAuthenticated;
             };
-    
+
             //authFac.getUsername = function() {
-                //return username;  
+                //return username;
             //};
 
             loadUserCredentials();
-    
+
             return authFac;
-    
+
         }])
 
 
         .factory('AdminAuthFactory', ['$resource', '$http', '$localStorage', '$rootScope', '$window', 'baseURL', 'ngDialog', function($resource, $http, $localStorage, $rootScope, $window, baseURL, ngDialog){
-    
+
             var authFac = {};
             var TOKEN_KEY = 'auth';
             var isAdminAuthenticated = false;
-    
+
 
             function loadUserCredentials() {
                 console.log("loadUserCredentials");
@@ -193,66 +193,28 @@ angular.module('pkuRunnerApp')
                     useCredentials(credentials);
                 }
             }
- 
+
             function storeUserCredentials(credentials) {
                 console.log("storeUserCredentials");
                 $localStorage.storeObject(TOKEN_KEY, credentials);
                 useCredentials(credentials);
             }
- 
+
             function useCredentials(credentials) {
                 console.log("useCredentials");
                 console.log(credentials);
                 isAdminAuthenticated = true;
             }
- 
+
             function destroyUserCredentials() {
                 isAdminAuthenticated = false;
                 $localStorage.remove(TOKEN_KEY);
                 console.log("destroyUserCredentials");
                 console.log("isAdminAuthenticated: " + isAdminAuthenticated);
             }
-     
-            authFac.preLogin = function(preLoginData) {
-                
-                console.log(preLoginData);
-                $resource(baseURL + "admin/login")
-                    .save({id:preLoginData.username},
-                          function(response) {
-                            console.log(response);
-                            if(response.data) {
-                                
-                                var code = response.data;
-                                console.log(code);
-                                var encrypted = CryptoJS.SHA256(preLoginData.password + code);
-                                console.log(encrypted.toString(CryptoJS.enc.hex));
-                                var loginData = {};
-                                loginData.id = preLoginData.username;
-                                loginData.password = encrypted.toString(CryptoJS.enc.hex);
-                                login(loginData);
-                            }
-                            else {
-                                var message = '<div class="ngdialog-message"><div><h3>Login Unsuccessful</h3></div>' +'<div><p>' +  response.code + '</p><p>' + response.message + '</p></div>' + '<div class="ngdialog-buttons"><button type="button" class="ngdialog-button ngdialog-button-primary" ng-click=confirm("OK")>OK</button></div>';
-                                ngDialog.openConfirm({ template: message, plain: 'true'});
-                                
-                                
-                            }
-                            
-                          },
-                          function(response){
-                            console.log(response);
-            
-                            var message = '<div class="ngdialog-message"><div><h3>Login Unsuccessful</h3></div>' +'<div><p>' +  response.data.err.message + '</p><p>' + response.data.err.name + '</p></div>' + '<div class="ngdialog-buttons"><button type="button" class="ngdialog-button ngdialog-button-primary" ng-click=confirm("OK")>OK</button></div>';
-            
-                            ngDialog.openConfirm({ template: message, plain: 'true'});
-                          }
-        
-                         );
-                
 
-            };
-            
-            function login(loginData) {
+            authFac.login = function(loginData) {
+
                 console.log(loginData);
                 $resource(baseURL + "admin/login")
                     .save(loginData,
@@ -262,7 +224,7 @@ angular.module('pkuRunnerApp')
                                 storeUserCredentials({id: loginData.id, password: loginData.password});
                                 //$rootScope.$broadcast('login:Successful');
                                 console.log("ok");
-                                window.location.href="http://pkuzone.jios.org:10201/dist/#!/admin";
+                                window.location.href="https://pkunewyouth.pku.edu.cn/public/dist/#!/admin";
                             }
                             else {
                                 var message = '<div class="ngdialog-message"><div><h3>Login Unsuccessful</h3></div>' +'<div><p>' +  response.code + '</p><p>' + response.message + '</p></div>' + '<div class="ngdialog-buttons"><button type="button" class="ngdialog-button ngdialog-button-primary" ng-click=confirm("OK")>OK</button></div>';
@@ -282,28 +244,30 @@ angular.module('pkuRunnerApp')
         
                          );
 
+
+
             };
-    
+
             authFac.logout = function() {
                 $resource(baseURL + "admin/logout").get(function(response){
                 });
                 destroyUserCredentials();
-                window.location.href="http://pkuzone.jios.org:10201/dist/#!/";
+                window.location.href="https://pkunewyouth.pku.edu.cn/public/dist/#!/";
             };
-    
-    
+
+
             authFac.isAdminAuthenticated = function() {
                 return isAdminAuthenticated
             };
-    
+
             //authFac.getUsername = function() {
-                //return username;  
+                //return username;
             //};
 
             //loadUserCredentials();
-    
+
             return authFac;
-    
+
         }])
 
 ;
